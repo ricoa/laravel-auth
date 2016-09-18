201ec7ffca53c2506b1a854a9e77a3282b92aa00
ricoo <ricoo@linkerlab.net>
2016-09-18 16:51:10 +0800

    feat:添加middleware

---
+++ src/Middlewares/Permission.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Ricoa\Auth\Middlewares;
+
+use Closure;
+use Illuminate\Contracts\Auth\Guard;
+use Route;
+
+class Permission
+{
+	protected $auth;
+
+	/**
+	 * Creates a new instance of the middleware.
+	 *
+	 * @param Guard $auth
+	 */
+	public function __construct(Guard $auth)
+	{
+		$this->auth = $auth;
+	}
+
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request $request
+	 * @param  Closure $next
+	 * @param  $permissions
+	 * @return mixed
+	 */
+	public function handle($request, Closure $next)
+	{
+        $action = str_replace("App\\Http\\Controllers\\","",Route::currentRouteAction());
+		if ($this->auth->guest() || (!$request->user()->hasRole(config('menus.super','super'))&&!$request->user()->can($action))) {
+			abort(403);
+		}
+
+		return $next($request);
+	}
+}
