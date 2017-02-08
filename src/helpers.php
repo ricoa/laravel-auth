cfb66d77d7940fd4e6a8ed7983cb7a67cc15ae04
ricoo <ricoo@linkerlab.net>
2017-02-08 16:56:42 +0800

    feat:优化控制器判断

--- src/helpers.php
+++ src/helpers.php
@@ -1,14 +1,14 @@
 <?php
 
 function can($user,$action){
-    //缓存
-    if(C($action)!==null){
-        return C($action);
-    }
-
     //判断权限
     $action = str_replace("App\\Http\\Controllers\\","",$action);
     $actions=explode("@",$action);
+
+    //缓存
+    if(C($actions[0])!==null){
+        return C($actions[0]);
+    }
 
     if(!$user->can($action)
         && !$user->can($actions[0]."@_ALL")//支持UserController@_ALL形式（匹配UserController@index..等等）
