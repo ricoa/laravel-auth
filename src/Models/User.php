c656dd9d45f9e46a5d68c99e319f1dab1c9fa608
ricoo <ricoo@linkerlab.net>
2017-02-16 16:55:35 +0800

    fix:补充User的trait

--- src/Models/User.php
+++ src/Models/User.php
@@ -3,8 +3,10 @@
 namespace Ricoa\Auth\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Zizaco\Entrust\Traits\EntrustUserTrait;
 
 class User extends Model
 {
+    use EntrustUserTrait;
     public $table = 'users';
 }
