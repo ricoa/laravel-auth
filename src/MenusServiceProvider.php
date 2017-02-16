5864be367b446b94c5cc9d4ea6097dd35844ac2d
ricoo <ricoo@linkerlab.net>
2017-02-16 16:55:11 +0800

    refactor:重构

--- src/MenusServiceProvider.php
+++ src/MenusServiceProvider.php
@@ -24,21 +24,32 @@ class MenusServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //publish a config file
+        //发布文件
+        $this->publishFiles();
+
+        //初始化菜单
+        $this->initializeMenus();
+    }
+
+
+    public function publishFiles()
+    {
         $this->publishes([
             __DIR__ . '/config/menus.php' => config_path('menus.php'),
         ], 'config');
 
-        //publish a blade file
         $this->publishes([
             __DIR__ . '/views/' => resource_path('views/'),
         ], 'config');
 
-        //publish resources files
         $this->publishes([
             __DIR__ . '/resources/' => public_path('app/'),
         ], 'config');
+    }
 
+
+    public function initializeMenus()
+    {
         $menu_blade=config('menus.blade','*.menu');
 
         require __DIR__ . '/routes.php';
